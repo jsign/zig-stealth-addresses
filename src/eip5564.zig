f8006997d2f3b4c8c3854427a42bb59d419337d3
Ignacio Hagopian <jsign.uy@gmail.com>
2023-12-18 08:44:21 -0300

    frontrun changing the scheme value to 1
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/eip5564.zig
+++ src/eip5564.zig
@@ -7,7 +7,7 @@ pub const Privkey = [32]u8;
 pub const Pubkey = [33]u8;
 pub const EthAddress = [20]u8;
 
-// StealthAddress is an implementation of EIP-5564 for the `schemeId`=0x00 (i.e: SECP256k1, with view tags)
+// StealthAddress is an implementation of EIP-5564 for the `schemeId`=0x01 (i.e: SECP256k1, with view tags)
 // In the future, we can generalize the code if more schemes are added.
 pub const EIP5564 = struct {
     // `n` as defined in the spec.
